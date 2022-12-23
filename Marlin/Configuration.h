ad4de5b1d0c2b1bce57bf7d4c70f5c3bbd684f21
Patrick de Bruijn <patrickdebruijn@gmail.com>
2022-12-23 20:57:30 +0100

    Lowered homing speeds

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2088,7 +2088,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (40*60), (40*60), (3*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
